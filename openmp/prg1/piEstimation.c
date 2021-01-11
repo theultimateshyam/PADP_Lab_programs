916999936cc11aefe6d0c2897523c70025bd0e4b
Shyam A <shyamasv.a@gmail.com>
2021-01-11 20:03:40 +0530

    change of srcshot

--- openmp/prg1/piEstimation.c
+++ openmp/prg1/piEstimation.c
@@ -3,35 +3,37 @@
 #include <stdlib.h>
 
 int main()
-{   srand(8861);
-    long n , i, count = 0;
+{
+    srand(8861);
+    long n, i, count = 0;
     double z, pi, x, y;
     int num_threads;
-    srand(omp_get_wtime());
-    double finishtime ;
-    double starttime = omp_get_wtime();
-    printf("Num of threads \tNum of pts   Value of Pi\n");
-    for (n=1000; n<=10000000; n= n*10){
+    // srand(omp_get_wtime());
+    double finishtime, starttime;
+    printf("Num of threads \tNum of pts \tTime Taken   Value of Pi\n");
+    for (n = 1000; n <= 10000000; n = n * 10)
+    {
 
-        for (num_threads = 1; num_threads <= 8; num_threads= num_threads*2){
-            printf("%d\t\t",num_threads);
+        for (num_threads = 1; num_threads <= 8; num_threads = num_threads * 2)
+        {
+            starttime = omp_get_wtime();
+            printf("%d\t\t", num_threads);
             count = 0;
-            #pragma omp private(x,y,z) FOR parallel num_threads(num_threads)
-                for (i = 0; i < n; i++)
-                {
-                    x = (double)rand() / RAND_MAX;
-                    y = (double)rand() / RAND_MAX;
-                    z = x * x + y * y;
-                    if (z <= 1)
-                        count++;
-                }
+#pragma omp private(x, y, z) FOR parallel num_threads(num_threads)
+            for (i = 0; i < n; i++)
+            {
+                x = (double)rand() / RAND_MAX;
+                y = (double)rand() / RAND_MAX;
+                z = x * x + y * y;
+                if (z <= 1)
+                    count++;
+            }
 
-                pi = (double)count / n * 4;
-                printf("%d\t%f \n", n, pi);
+            pi = (double)count / n * 4;
+            finishtime = omp_get_wtime() - starttime;
+            printf("%d\t\t%f \t%f \n", n, finishtime, pi);
         }
     }
-    finishtime = omp_get_wtime();
-    printf("\nExecution time is : %f", (double)(finishtime - starttime));
 
     return 0;
 }

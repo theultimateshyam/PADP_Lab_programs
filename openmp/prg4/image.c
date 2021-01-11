82cb5642bc3b115c85d7d41033116bd5187e3d14
Shyam A <shyamasv.a@gmail.com>
2021-01-11 22:10:29 +0530

    added graphs and minor changes

--- openmp/prg4/image.c
+++ openmp/prg4/image.c
@@ -1,52 +1,75 @@
 #include <stdio.h>
-#include <error.h>
+#include <omp.h>
 #include <gd.h>
-#include<string.h>
-int main(intargc, char **argv) 
+int main(int argc, char *argv[])
 {
- FILE *fp = {0}; 
-gdImagePtrimg;
-char *iname = "in.png";
-char *oname = "out.png";
-intcolor, x, y, w, h,i=0;
-int red, green, blue;
-color = x = y = w = h = 0;
-red = green = blue = 0;
-if(argc != 3)
-error(1, 0, "Usage: gdnegat input.png output.png");
-else
-{
-iname = argv[1];
-oname = argv[2];
- }
-if((fp = fopen(iname, "r")) == NULL)
-error(1, 0, "Error - fopen(): %s", iname);
-else
-img = gdImageCreateFromPng(fp);
-w = gdImageSX(img);
-h = gdImageSY(img);
-double t = omp_get_wtime();
-for(x = 0; x < w; x++) {
-for(y = 0; y < h; y++) {
-color = x+0;
-color=gdImageGetPixel(img, x, y);
-red   = 255 - gdImageRed(img, color);
-green = 255 - gdImageGreen(img, color);
-blue  = 255 - gdImageBlue(img, color);
-color = gdImageColorAllocate(img, red, green, blue);
-gdImageSetPixel(img, x, y, color);
-}
-}
-if((fp = fopen(oname, "w")) == NULL)
-error(1, 0, "Error - fopen(): %s", oname);
-else
- {
-gdImagePng(img, fp);
-fclose(fp);
-}
-t = omp_get_wtime() - t;
-gdImageDestroy(img);
-printf("Time taken = %g",t);
-return 0;
-}
+  int nt = 4;
+  int tid, tmp, red, green, blue, color, x, h, y, w;
+  tmp = red = green = blue = color = x = h = y = w = 0;
+  char *iname = "in.png";
+  char *oname = "out.png";
+  gdImagePtr img;
+  FILE *fp = {0};
+  /*
+    if(argc!=3)
+        error(1,0,"format : object_file input.png output.png");
+    else
+    {*/
+  //iname = "in.png";
+  //oname = "out.png";
+  //}
+  if ((fp = fopen(iname, "r")) == NULL)
+    printf("file error");
+  else
+  {
+    img = gdImageCreateFromPng(fp);
+    w = gdImageSX(img);
+    h = gdImageSY(img);
+  }
+  double t = omp_get_wtime();
+  omp_set_num_threads(nt);
+#pragma omp parallel for private(y, color, red, blue, green) schedule(dynamic, 50) /*schedule(dynamic,50)  schedule(guided)*/
+  for (x = 0; x < w; x++)
+    for (y = 0; y < h; y++)
+    {
+      tid = omp_get_thread_num();
 
+      color = gdImageGetPixel(img, x, y);
+      red = gdImageRed(img, color);
+      green = gdImageGreen(img, color);
+      blue = gdImageBlue(img, color);
+      tmp = (red + green + blue) / 3;
+      red = green = blue = tmp;
+      if (tid == 0)
+      {
+        color = gdImageColorAllocate(img, red, 0, 0);
+        gdImageSetPixel(img, x, y, color);
+      }
+      if (tid == 1)
+      {
+        color = gdImageColorAllocate(img, 0, green, 0);
+        gdImageSetPixel(img, x, y, color);
+      }
+      if (tid == 2)
+      {
+        color = gdImageColorAllocate(img, 0, 0, blue);
+        gdImageSetPixel(img, x, y, color);
+      }
+      if (tid == 3)
+      {
+        color = gdImageColorAllocate(img, 0, 0, 0);
+        gdImageSetPixel(img, x, y, color);
+      }
+    }
+  t = omp_get_wtime() - t;
+  printf("\ntime taken : %lf threads : %d", t, nt);
+  if ((fp = fopen(oname, "w")) == NULL)
+    printf("file error out");
+  else
+  {
+    gdImagePng(img, fp);
+    fclose(fp);
+  }
+  gdImageDestroy(img);
+  return 0;
+}
